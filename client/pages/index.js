b366a8a910e9bd3c89cce64ac176b0225571b2fd
Max Euler <maxjuliuseuler@gmail.com>
2019-11-23 19:01:10 +0100

    initial

---
+++ client/pages/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import MessageList from '../components/messageList';
+
+const Home = () => (
+  <div>
+    <h3>Hello</h3>
+    <MessageList></MessageList>
+  </div>
+);
+
+export default Home;
---
+++ server/db.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const connectToDB = url =>
+  mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
+
+mongoose.connection.on('connected', () => {
+  console.log('🚀 🚀  connected to mongo');
+});
+
+module.exports = connectToDB;
